5d1c5540b73155f3b6f1d5cd587c089c58328ed6
Kilo Loco <kiloloco@me.com>
2020-09-03 19:28:41 -0700

    Initial Commit

---
+++ CrissCross/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrissCross
+//
+//  Created by Kyle Lee on 9/3/20.
+//  Copyright © 2020 Kilo Loco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
